55ec72e4a5cb5265373a355ed294ffbde9733ee9
GiangLong2520 <you@long.dinhgianglong@gmail.com>
2023-07-20 21:56:14 +0700

    update

--- HospitalServiceApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HospitalServiceApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
